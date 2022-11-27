f56d0247ed8aafe41693063a81bf8baf06ff8407
annavishnyakova <vishnyakova0307@gmail.com>
2022-11-27 16:21:28 +0300

    complex refactoring

--- test_example.py
+++ test_example.py
@@ -1,5 +1,8 @@
-def test_example():
-    assert 3 > 1
+def test_example_0():
+    assert 10 > 1
 
-def test_conflict_dz1():
-    assert 0 == 0
+def test_conflict_dz1_1():
+    assert 0 != 1
+
+def test_conflict_dz1_2():
+        assert 0 != 1
